83f4f99ab9fb7ecc700c22d7b1b751d34fb288c4
hazzzi <hanee_l@naver.com>
2019-12-19 15:59:46 +0900

    JPA auditing 의존성 추가 / 모델 수정

--- src/main/kotlin/server/app/domain/model/Todo.kt
+++ src/main/kotlin/server/app/domain/model/Todo.kt
@@ -1,5 +1,6 @@
 package server.app.domain.model
 
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
@@ -11,16 +12,15 @@ data class Todo constructor(
 
     @Id
     @GeneratedValue
-    var todoId: Long,
+    var todoId: Long?,
 
     @Column(nullable = true)
     val contents: String,
 
-//    @org.hibernate.annotations.Type(type = "server.base.hibernate.type.ZeroOrOneType")
     @Column(nullable = false)
     val isComplete: Boolean,
 
-    @LastModifiedDate
+    @CreatedDate
     @Column(nullable = false)
     var createTimestamp: Instant? = null,
 
